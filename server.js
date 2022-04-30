db8980be847308f3a8864b7c7db1223b6c06aa0f
Yakov P <yakov.petryk@gmail.com>
2022-04-30 17:59:34 +0300

    feat: add cors middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const Koa = require("koa");
 const BodyParser = require("koa-bodyparser");
+const cors = require('@koa/cors');
+
 
 const todoRouter = require("./routes/todosRoutes");
 const logger = require('./middleware/logger')
@@ -7,6 +9,7 @@ const app = new Koa();
 
 const PORT = process.env.PORT || 4000;
 
+app.use(cors())
 app.use(BodyParser());
 app.use(todoRouter.routes()).use(todoRouter.allowedMethods());
 app.use(logger)
